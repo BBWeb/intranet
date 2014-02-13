d5caef76558561edc775c0d050ccb2ecdd915d82
Niklas Andreasson <eaglus_@hotmail.com>
2014-02-13 14:10:39 +0100

    Changed indentation in manage-staff.js

--- public/js/manage-staff.js
+++ public/js/manage-staff.js
@@ -1,29 +1,30 @@
 ;(function() {
 
-  var $trParent;
+   var 
+   var $trParent;
 
    $('#staff-members').on('click', '.remove-button', function() {
       $trParent = $(this).closest('tr');
 
-     $('#remove-staff-modal').modal();
+      $('#remove-staff-modal').modal();
    });
 
-    $('.sucess-button').click(function() {
+   $('.sucess-button').click(function() {
 
       $('#remove-staff-modal').modal('hide');
 
       var id = $trParent.data('id')
 
       $.ajax({
-        url: '/staff/' + id,
-        type: 'DELETE',
+         url: '/staff/' + id,
+         type: 'DELETE',
 
-        success: function(data, textStatus) {
-          if( textStatus !== 'success' ) return;
+         success: function(data, textStatus) {
+            if( textStatus !== 'success' ) return;
 
-          $trParent.hide();
+            $trParent.hide();
          }
-       });
-    });
+      });
+   });
 
 })();
