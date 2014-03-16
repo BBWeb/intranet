438bbf4f36a991f5f7094961acdc1e8a2f2eb0f2
Niklas Andreasson <eaglus_@hotmail.com>
2014-03-16 14:18:27 +0100

    Include new datepicker script

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -15,7 +15,7 @@
   {{ HTML::script('/js/libs/jquery-2.0.3.js') }}
   {{ HTML::script('/js/libs/bootstrap.js') }}
   {{ HTML::script('/js/libs/underscore-min.js') }}
-    {{ HTML::script('/js/libs/datepicker.js') }}
+    {{ HTML::script('/js/libs/bootstrap-datepicker.js') }}
    @yield('footer-scripts')
 </body>
 </html>
