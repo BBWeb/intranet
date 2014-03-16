9d69681ff3e1ec5675b0142231a13b1ec0f78017
Niklas Andreasson <eaglus_@hotmail.com>
2014-03-16 14:14:00 +0100

    Add filter for checking if apikey is set

--- app/routes.php
+++ app/routes.php
@@ -12,27 +12,28 @@
 */
 
 Route::group(array('before' => 'auth'), function() {
-
-   Route::get('/', 'ProjectsController@getIndex');
    Route::controller('account', 'AccountController');
+   Route::get('reported-time', 'ReportedTimeController@getIndex');
 
-   Route::post('task-remove', 'TaskController@delete');
+   Route::group(array('before' => 'apikey'), function() {
+      Route::get('/', 'ProjectsController@getIndex');
 
-   Route::resource('task', 'TaskController');
-   Route::post('task-update-time', 'TaskController@updateTime');
-   Route::post('task-report', 'TaskController@report');
+      Route::resource('task', 'TaskController');
+      Route::post('task-remove', 'TaskController@delete');
+      Route::post('task-update-time', 'TaskController@updateTime');
+      Route::post('task-report', 'TaskController@report');
 
-   Route::resource('asana', 'AsanaController');
-
-   Route::get('/reported-time', 'ReportedTimeController@getIndex');
-
+      Route::resource('asana', 'AsanaController');
+   });
 });
 
 Route::group(array('before' => 'admin'), function() {
    Route::resource('staff', 'AdminManageStaffController');
 
    Route::resource('time', 'AdminReportedTimeController');
+
    Route::get("customer-report","CustomerViewController@getIndex");
+   Route::get("customer-report/{project}/{from}/{to}", "CustomerViewController@getProjectOverview");
 });
 
 Route::get('login', 'AuthController@getLogin');
