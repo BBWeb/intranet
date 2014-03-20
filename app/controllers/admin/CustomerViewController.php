8bd27dd206e277b132779e47a4afa9f93b44b1dc
Niklas Andreasson <eaglus_@hotmail.com>
2014-03-20 22:44:09 +0100

    Add function in controller for print view

--- app/controllers/admin/CustomerViewController.php
+++ app/controllers/admin/CustomerViewController.php
@@ -36,4 +36,32 @@ class CustomerViewController extends BaseController {
 		);
 	}
 
+	public function printProjectOverview($projectId, $fromStr, $toStr)
+	{
+		$project = Project::find($projectId);
+
+	    $from = date('Y-m-d', strtotime($fromStr));
+  		$to = date('Y-m-d', strtotime($toStr));
+	    $projectTasks = $project->tasks()->whereBetween('reported_date', array($from, $to))->get();
+
+	    $totalTime = 0;
+	    // go through all "adjusted time wich are not set"
+	    foreach ($projectTasks as $projectTask) {
+
+	    	if ( $projectTask->adjusted_time == 0) {
+	    		$projectTask->adjusted_time = $projectTask->time_worked;
+	    		$projectTask->save();
+	    	}
+	    	$totalTime += $projectTask->adjusted_time;
+	    }
+
+	    return View::make('admin.customer-report.print', array(
+	    	'from' => $from,
+	    	'to' => $to,
+	    	'totalTime' => $totalTime,
+	    	'project' => $project,
+	    	'tasks' => $projectTasks
+    	));
+	}
+
 }
