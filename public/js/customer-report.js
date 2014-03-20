2f01e635d29585e1620dc1683d68e6d673aa64a1
Niklas Andreasson <eaglus_@hotmail.com>
2014-03-20 22:42:46 +0100

    Add functionality to update adjusted time

--- public/js/customer-report.js
+++ public/js/customer-report.js
@@ -16,3 +16,19 @@ $('#search-project-tasks').click(function(e) {
 
   $(location).attr('href', url);
 });
+
+$('#project-tasks').on('change', 'input.adjusted-time', function() {
+  var $adjustedTimeInput = $(this)
+    , $trParent = $adjustedTimeInput.closest('tr')
+    , id = $trParent.data('id')
+    , adjustedTime = $adjustedTimeInput.val()
+    ;
+
+  $adjustedTimeInput.after('<span class="spinner"></span>');
+  // TODO mock put request
+  $.post('/task/update-adjusted-time', { id: id, 'adjusted-time': adjustedTime }, function(data, textStatus, jqXhr) {
+     if (textStatus !== 'success') return;
+
+     $adjustedTimeInput.siblings('.spinner').remove();
+  });
+   });
