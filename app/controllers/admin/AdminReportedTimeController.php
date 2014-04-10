b683ee986968b0583df1587f15a535862ca5e846
Niklas Andreasson <eaglus_@hotmail.com>
2014-04-10 10:59:24 +0200

    calculate totaltime and pass to view, add method for getting payed tasks for user

--- app/controllers/admin/AdminReportedTimeController.php
+++ app/controllers/admin/AdminReportedTimeController.php
@@ -9,21 +9,32 @@ class AdminReportedTimeController extends BaseController {
 		return \View::make('admin.staff-report.base')->with(array('users' => $users));
 	}
 
-	public function getTimeReport($userId)
+	public function getTimeReport($userId, $fromStr, $toStr)
 	{
 		$users = User::all();
 
 		$user = User::find($userId);
 
+	    $from = date('Y-m-d', strtotime($fromStr));
+  		$to = date('Y-m-d', strtotime($toStr));
+
 		// add option for admin to mark tasks as "payed"
     	// TODO we should only get tasks with "unpayed" subreports
     	// This also means that the "totaltime" that we want to see in this view is unreported time
-		$unpayedTasks = $user->unpayedTasks();
+		$unpayedTasks = $user->unpayedTasksBetween($from, $to);
+
+		$totalTime = 0;
+		foreach ($unpayedTasks as $unpayedTask) {
+			$totalTime += $unpayedTask->totalUnpayedTime();
+		}
 
 		return View::make('admin.staff-report.index',
 			array(
         		'users' => $users,
-				'tasks' => $unpayedTasks
+				'tasks' => $unpayedTasks,
+				'from' => $from,
+				'to' => $to,
+				'totaltime' => $totalTime
 			)
 		);
 	}
@@ -33,6 +44,19 @@ class AdminReportedTimeController extends BaseController {
     $users = User::all();
 
     return View::make('admin.staff-report.payed', array('users' => $users));
+  }
+
+  public function getPayedUser($userId)
+  {
+    $users = User::all();
+  	$user = User::find($userId);
+
+  	$payedTasks = $user->payedTasks();
+
+    return View::make('admin.staff-report.payed-report', array(
+    	'users' => $users,
+      'tasks' => $payedTasks
+  	));
   }
 
 }
