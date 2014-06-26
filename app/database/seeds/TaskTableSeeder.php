65155b2100eaf2d898fb32e754b4707e0bd1c024
Niklas Andréasson <andreasson.n@gmail.com>
2014-06-26 14:22:50 +0200

    Add more data to seeders, new users, tasks etc

--- app/database/seeds/TaskTableSeeder.php
+++ app/database/seeds/TaskTableSeeder.php
@@ -7,7 +7,25 @@ class TaskTableSeeder extends Seeder {
 		DB::table('tasks')->delete();
 
       // find niklas
-      $user = DB::table('users')->where('email', 'niklas@bbweb.se')->first();
+		$user = DB::table('users')->where('email', 'niklas@bbweb.se')->first();
+
+		// add a task
+		$task = Task::create(array(
+			'user_id' => $user->id,
+			'asana_id' => '1',
+			'project_id' => 1,
+		));
+
+		// date
+		$dateTimestamp = mktime(0, 0, 0, 6, 11, 2014);
+		$date = date('Y-m-d', $dateTimestamp);	
+
+		$subreport = Subreport::create(array(
+			'task_id' => $task->id,
+			'time' => 31,
+			'reported_date' => $date
+			)
+		); 
 
 	}
 }
