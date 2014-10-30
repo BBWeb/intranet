d36e583b7e67d47596d81c4b54e5a1129045fbc4
Niklas Andréasson <andreasson.n@gmail.com>
2014-10-30 15:30:28 +0100

    Add method to Taskpresenter for formatting time into hh:mm

--- app/Intranet/Presenters/TaskPresenter.php
+++ app/Intranet/Presenters/TaskPresenter.php
@@ -44,4 +44,31 @@ class TaskPresenter extends BasePresenter {
     return $asanaTask->project->name;
   }
 
+  public function formattedUnpayedTime()
+  {
+    $totalUnpayedTime = $this->resource->totalUnpayedTime();
+
+    return $this->formatTime($totalUnpayedTime);
+  }
+
+  public function formattedTotalTime()
+  {
+    $totalTime = $this->resource->totaltime();
+
+    return $this->formatTime($totalTime);
+  }
+
+  /**
+   *  Formats time to HH:mm
+   * @param  integer $time in minutes
+   * @return String      Formatted time
+   */
+  private function formatTime($time)
+  {
+    $hours = floor($time / 60);
+    $minutes = $time % 60;
+
+    return ($hours < 9 ? '0' : '') . $hours . ':' . $minutes . ($minutes < 9 ? '0' : '');
+  }
+
 }
